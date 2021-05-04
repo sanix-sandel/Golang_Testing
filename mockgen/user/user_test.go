76e31ac732c33e296f0fb1e689e85919bef677d4
sanix <sanicksikani@gmail.com>
2021-05-04 21:25:19 +0300

    Creating dummy error

--- mockgen/user/user_test.go
+++ mockgen/user/user_test.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/Go_Testing/Golang_Testing/mockgen/mocks"
@@ -19,4 +20,24 @@ func TestUser(t *testing.T) {
 	mockDoer.EXPECT().DoSomething(123, "Hello GoMock").Return(nil).Times(1)
 
 	testUser.Use()
+}
+
+func TestUseReturnsErrorFromDo(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	dummyError := errors.New("dummy error")
+	mockDoer := mocks.NewMockDoer(mockCtrl)
+	testUser := &User{Doer: mockDoer}
+
+	//Expect Do to be called once with 123 and "Hello GoMock"
+	//as params, and return dummyError from the mocked call
+	mockDoer.EXPECT().DoSomething(123, "Hello GoMock").Return(dummyError).Times(1)
+
+	err := testUser.Use()
+
+	if err != nil {
+		t.Fail()
+	}
+
 }
